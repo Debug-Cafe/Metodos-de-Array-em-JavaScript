3879190eac5a2065019561203b4709266d17b1a7
nathashabrito <britonathasha@gmail.com>
2025-07-28 00:03:28 -0300

    exercicio 2-c

---
+++ exercicio2/exercicio2-c.js
@@ -0,0 +1,15 @@
+const products = [
+  { id: 1, name: "Notebook", price: 1200, category: "Eletrônicos" },
+  { id: 2, name: "Camiseta", price: 25, category: "Vestuário" },
+  { id: 3, name: "Cafeteira", price: 100, category: "Cozinha" },
+  { id: 4, name: "Fones de Ouvido", price: 80, category: "Eletrônicos" },
+  { id: 5, name: "Calça Jeans", price: 45, category: "Vestuário" },
+];
+
+let totalPrice = 0;
+
+products.forEach(product => {
+  totalPrice += product.price;
+});
+
+console.log(`c. Valor total dos produtos: R$${totalPrice.toFixed(2)}`);
