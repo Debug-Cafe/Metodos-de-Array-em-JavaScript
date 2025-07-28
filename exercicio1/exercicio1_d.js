f89467be26b8c0de0ec529ee89d82c9a9873fc7b
Jusie <barbosa.providencial@gmail.com>
2025-07-28 13:26:59 -0300

    Exercicio1_d

---
+++ exercicio1/exercicio1_d.js
@@ -0,0 +1,6 @@
+const timesPaulistas = ['Corinthians', 'São Paulo', 'Palmeiras'];
+const timesCariocas = ['Flamengo', 'Vasco', 'Botafogo'];
+
+const todosOsTimes = [...timesPaulistas, ...timesCariocas];
+
+console.log(todosOsTimes);
